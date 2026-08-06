f7c6ad05af6ced032f599e88fa5dbd4d0800ccf6
zildjianvitoo <zildjianvito07@gmail.com>
2026-08-06 11:37:44 +0700

    docs: add initial PRD and contributing guidelines

---
+++ holdy/holdyApp.swift
@@ -0,0 +1,17 @@
+//
+//  holdyApp.swift
+//  holdy
+//
+//  Created by Zildjian Vito  on 06/08/26.
+//
+
+import SwiftUI
+
+@main
+struct holdyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
