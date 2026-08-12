c5650f789cf28c079bd53838b39bb707cfa80122
muhammadmuttakin <muttakin2418@gmail.com>
2026-08-12 12:04:17 +0700

    onboarding-1-2-update

--- buydee/Features/Onboarding/Views/SupportViews/OnboardingPage1View.swift
+++ buydee/Features/Onboarding/Views/SupportViews/OnboardingPage1View.swift
@@ -10,34 +10,41 @@ import SwiftUI
 struct OnboardingPage1View: View {
     var action: () -> Void
     var body: some View {
-        VStack {
-            Text("Hi, There!\nI’ll be your personal shopping buddy.")
-                .font(Font.largeTitle.bold())
-                .foregroundStyle(Color(red: 82 / 255, green: 54 / 255, blue: 2 / 255))
+        ZStack{
+            Image("BG-onboarding-1")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Hi, There!\nI’ll be your personal shopping buddy.")
+                    .font(Font.buydeeLargeTitle)
+                    .foregroundStyle(Color.buydee.primaryText)
+                    .padding(.top, 100)
+                Spacer()
+                
+                ZStack {
+                    Image("otter")
 
-            ZStack {
-                Image("otter")
+                }
+                Spacer()
+                
+                Button(action: action) {
+                    Text("Bring me along")
+                        .font(Font.buydeeHeadline)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, minHeight: 48)
+                        .background(Color.buydee.primaryButton)
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: 16)
+                        )
+                }
+                .padding(.horizontal, 50)
+                .padding(.bottom, 150)
 
-                Ellipse()
-                    .fill(Color.blue.opacity(0.1))
-                    .frame(width: 700, height: 450)
-                    .offset(x: 0, y: 400)
             }
-
-            Button(action: action) {
-                Text("Bring me along")
-                    .font(.headline)
-                    .fontWeight(.semibold)
-                    .foregroundColor(.white)
-                    .frame(width:250, height: 48)
-                    .background(Color(red: 84/255, green: 88/255, blue: 26/255))
-                    .clipShape(
-                        RoundedRectangle(cornerRadius: 16)
-                    )
-            }
-            .padding(.horizontal, 50)
-            .padding(.bottom, 40)
         }
+        
         
     }
 }
--- buydee/Features/Onboarding/Views/SupportViews/OnboardingPage2View.swift
+++ buydee/Features/Onboarding/Views/SupportViews/OnboardingPage2View.swift
@@ -10,72 +10,76 @@ import SwiftUI
 struct OnboardingPage2View: View {
     var action: () -> Void
     var body: some View {
-        VStack {
-            Text("Let's take another look before you buy!")
-                .font(.largeTitle.bold())
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.top, 24)
-                .foregroundStyle(Color(red: 82 / 255, green: 54 / 255, blue: 2 / 255))
-
-            Text("I’ll be here whenever you’re thinking\nabout buying something!")
-                .font(.title3)
-                .padding(.top, 2)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .foregroundStyle(Color(red: 82 / 255, green: 54 / 255, blue: 2 / 255))
-        
-            ZStack {
-                VStack(spacing: 3) {
-                    iconCircle(icon: "hat.widebrim.fill")
-
-                    HStack {
-                        iconCircle(icon: "bag.fill")
-
-                        Spacer()
-
-                        iconCircle(icon: "sunglasses")
-                    }
-                    .padding(.horizontal, 20)
-                }
-                .padding(.bottom, 150)
-                // MARK: - Otter
-
-                Image("otter")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 150)
-                    .offset(y: 100)
-            }
-            .frame(height: 450)
+        ZStack {
+            Image("BG-onboarding-2")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
             
-            Spacer()
+            VStack {
+                VStack {
+                    Text("Let's take another look before you buy!")
+                        .font(Font.buydeeLargeTitle)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundStyle(Color.buydee.primaryText)
 
-            Button(action: action) {
-                Text("Show Me")
-                    .font(.headline)
-                    .fontWeight(.semibold)
-                    .foregroundColor(.white)
-                    .frame(width:250, height: 48)
-                    .background(Color(Color(red: 84/255, green: 88/255, blue: 26/255)))
-                    .clipShape(
-                        RoundedRectangle(cornerRadius: 16)
-                    )
+                    Text("I’ll be here whenever you’re thinking about buying something!")
+                        .font(Font.buydeeTitle3)
+                        .padding(.top, 2)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundStyle(Color.buydee.primaryText)
+                }
+                .padding(.bottom, 30)
+
+                ZStack {
+                    VStack {
+                        iconCircle(icon: "hat.widebrim.fill")
+
+                        HStack {
+                            iconCircle(icon: "bag.fill")
+
+                            Spacer()
+
+                            iconCircle(icon: "sunglasses")
+                        }
+                        .padding(.horizontal, 20)
+                    }
+                    .padding(.bottom, 250)
+
+                    Image("otter")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150)
+                        .offset(y: 10)
+                }
+                
+                VStack {
+                    Button(action: action) {
+                        Text("Show Me")
+                            .font(Font.buydeeHeadline)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, minHeight: 48)
+                            .background(Color.buydee.primaryButton)
+                            .clipShape(
+                                RoundedRectangle(cornerRadius: 16)
+                            )
+                    }
+                }
+                .padding(.bottom, 50)
             }
-            .padding(.horizontal, 50)
-            .padding(.bottom, 40)
+            .padding(.horizontal, 30)
         }
-        .padding(.horizontal, 30)
     }
-
 
     func iconCircle(icon: String) -> some View {
         ZStack {
             Circle()
-                .fill(Color(red: 212 / 255, green: 219 / 255, blue: 129 / 255 ))
+                .fill(Color.buydee.background)
                 .frame(width: 76, height: 76)
 
             Image(systemName: icon)
                 .font(.system(size: 30))
-                .foregroundColor(Color(red: 82 / 255, green: 54 / 255, blue: 2 / 255))
+                .foregroundColor(Color.buydee.primaryText)
         }
     }
 }
