9efa83ce397a80f4a03cff0d9b8d106f9c1739bc
Ikzaaz Bakhtar Abdurrahman <ikzaazabd@gmail.com>
2026-08-13 10:35:58 +0700

    refactor: onboarding swipe screen behaviour

--- buydee/Features/Onboarding/Views/OnboardingView.swift
+++ buydee/Features/Onboarding/Views/OnboardingView.swift
@@ -5,38 +5,35 @@
 import SwiftUI
 struct OnboardingView: View {
     @State private var viewModel = OnboardingViewModel()
+
     var body: some View {
-        TabView(selection: $viewModel.currentPage) {
-            
-            
+        ZStack {
+            Color.buydee.canvasBackground
+                .ignoresSafeArea()
+
+            switch viewModel.currentPage {
+            case 0:
             OnboardingPage1View(action: {
                 viewModel.nextPage()
             })
-            .tag(0)
-            
-//            OnboardingPage2View(action: {
-//                viewModel.nextPage()
-//            })
-//            .tag(1)
-            
+
+            case 1:
             OnboardingPage3View(action: {
                 viewModel.nextPage()
             })
-            .tag(1)
-        
+
+            case 2:
             OnboardingPage4View(viewModel: viewModel, action: {
                 viewModel.nextPage()
             })
-            .tag(2)
 
+            default:
             OnboardingPage5View(action: {
                 viewModel.completeOnboarding()
             })
-            .tag(3)
+            }
         }
-        .tabViewStyle(.page(indexDisplayMode: .never))
         .animation(.easeInOut, value: viewModel.currentPage)
-        .ignoresSafeArea()
     }
 }
 #Preview {
